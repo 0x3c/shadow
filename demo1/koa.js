f5b1b8ea94c9e782920344590a6247b290d576be
SamuelChia <funwithes6@gmail.com>
2018-05-29 22:01:27 +0800

    feat: test fetch

--- demo1/koa.js
+++ demo1/koa.js
@@ -2,14 +2,23 @@ const Koa = require("koa");
 const route = require("koa-route");
 const app = new Koa();
 
-const main = ctx => {
+const main = (ctx, next) => {
+  console.log(1);
   ctx.response.type = "json";
   ctx.response.body = "Hello Koa";
+  next();
+};
+const main2 = (ctx, next) => {
+  console.log(2);
+  ctx.response.type = "json";
+  ctx.response.body = "Hello Koa";
+  next();
 };
 const help = ctx => {
   ctx.response.type = "json";
   ctx.response.body = "Help Koa";
 };
 app.use(route.get("/", main));
+app.use(route.get("/", main2));
 app.use(route.get("/help", help));
 app.listen(9966);
